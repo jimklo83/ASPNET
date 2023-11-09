a7d389f8b2ffa5a7f66a500fc631bbbb05832f4a
jimklo83 <jimklo83@mail.com>
2023-11-09 13:53:16 -0600

    Completed all sections

--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -14,7 +14,8 @@ namespace Testing
         {
             _conn = conn;
         }
-        public IEnumerable<Product> GetAllProducts()
+
+		public IEnumerable<Product> GetAllProducts()
         {
             return _conn.Query<Product>("SELECT * FROM products;");
         }
@@ -28,6 +29,32 @@ namespace Testing
 		{
             _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
                 new { name = product.Name, price = product.Price, id = product.ProductID });
+		}
+
+		public void InsertProduct(Product productToInsert)
+		{
+			_conn.Execute("INSERT INTO products (Name, Price, CategoryID) VALUES (@name, @price, @categoryID);)",
+				new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+		}
+
+		public IEnumerable<Category> GetCategories()
+		{
+			return _conn.Query<Category>("SELECT * FROM categories;");
+		}
+
+		public Product AssignCategory()
+		{
+			var categoryList = GetCategories();
+			var product = new Product();
+			product.Categories = categoryList;
+			return product;
+		}
+
+		public void DeleteProduct(Product product)
+		{
+			_conn.Execute("DELETE FROM reviews WHERE ProductID = @id;", new { id = product.ProductID });
+			_conn.Execute("DELETE FROM sales WHERE ProductID = @id;", new { id = product.ProductID });
+			_conn.Execute("DELETE FROM products WHERE ProductID = @id;", new { id = product.ProductID });
 		}
 	}
 }
